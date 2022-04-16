d2eae30f523678f5314b2aac34c98f862c698d9b
Ghaarp <xdmail@bk.ru>
2022-04-16 13:08:40 +0300

    Added sources

---
+++ client/src/utils/consts.js
@@ -0,0 +1,8 @@
+export const MAINPAGE_ROUTE = '/';
+export const NEWGAME_ROUTE = '/play';
+export const BESTRESULTS_ROUTE = '/top';
+export const LOGIN_ROUTE = '/login';
+export const REG_ROUTE = '/registration';
+
+export const TOKEN_STORAGE_KEY = "CardsGame_token";
+export const REACT_APP_API_URL='http://localhost:7000/'
